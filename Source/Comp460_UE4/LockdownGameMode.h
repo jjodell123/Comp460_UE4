244755a43c52cf994ef92008e2b2ed0868c3ba3a
jjodell123 <jjodell123@gmail.com>
2021-03-25 13:47:48 -0500

    Added in players and networking

---
+++ Source/Comp460_UE4/LockdownGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LockdownGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMP460_UE4_API ALockdownGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+	public:
+	ALockdownGameMode();
+	
+};
