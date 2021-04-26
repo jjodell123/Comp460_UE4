0979b19f78b2433832584272c7ba3e6a898fc1c1
jjodell123 <jjodell123@gmail.com>
2021-04-25 19:45:52 -0500

    Updated networking mechanics

--- Source/Comp460_UE4/MyProjectile.cpp
+++ Source/Comp460_UE4/MyProjectile.cpp
@@ -11,25 +11,25 @@ AMyProjectile::AMyProjectile()
 {
     // Die after 3 seconds by default
     InitialLifeSpan = 1.0f;
-    
-     // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = false;
-    
+
     //Use a sphere as a simple collision representation
     CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
-        
+
     // Set the sphere's collision profile name to "Projectile".
     CollisionComponent->BodyInstance.SetCollisionProfileName(TEXT("Projectile"));
-        
+
     // Event called when component hits something.
     CollisionComponent->OnComponentHit.AddDynamic(this, &AMyProjectile::OnHit);
-        
+
     //Set the sphere's collisiion radius.
     CollisionComponent->InitSphereRadius(15.0f);
-        
+
     //Set the root component to be the collision component
     RootComponent = CollisionComponent;
-    
+
     // Use a ProjectileMovementComponent to govern this projectile's movement
     ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
     ProjectileMovementComponent->SetUpdatedComponent(CollisionComponent);
@@ -39,14 +39,14 @@ AMyProjectile::AMyProjectile()
     ProjectileMovementComponent->bShouldBounce = true;
     ProjectileMovementComponent->Bounciness = 0.5f;
     ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
-    
+
     ProjectileMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMeshComponent"));
     static ConstructorHelpers::FObjectFinder<UStaticMesh>Mesh(TEXT("'/Game/Blueprints/Sphere.Sphere'"));
-    if(Mesh.Succeeded())
+    if (Mesh.Succeeded())
     {
         ProjectileMeshComponent->SetStaticMesh(Mesh.Object);
     }
-        
+
     static ConstructorHelpers::FObjectFinder<UMaterial>Material(TEXT("'/Game/Materials/Items/SphereMaterial.SphereMaterial'"));
     if (Material.Succeeded())
     {
@@ -55,7 +55,7 @@ AMyProjectile::AMyProjectile()
     ProjectileMeshComponent->SetMaterial(0, ProjectileMaterialInstance);
     ProjectileMeshComponent->SetRelativeScale3D(FVector(0.09f, 0.09f, 0.09f));
     ProjectileMeshComponent->SetupAttachment(RootComponent);
-    
+
     bReplicates = true;
 }
 
