3a6644a31c5a0732c89bfaca40cc7ea43f1bc9dd
Riceloaner <Riceloaner@OITLOAN7bdn793.ad.rice.edu>
2021-04-26 02:25:19 -0500

    message this time

--- Source/Comp460_UE4/PlayerCharacter.cpp
+++ Source/Comp460_UE4/PlayerCharacter.cpp
@@ -15,88 +15,82 @@
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
-	// Disables in built controller rotation
-	bUseControllerRotationYaw = true;
-	bUseControllerRotationPitch = true;
-	bUseControllerRotationRoll = false;
-    
-//    FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
-////    FP_MuzzleLocation->SetupAttachment(FP_Gun);
-//    FP_MuzzleLocation->SetRelativeLocation(FVector(42.0f, 40.0f, 40.0f));
+    // Disables in built controller rotation
+    bUseControllerRotationYaw = false;
+    bUseControllerRotationPitch = false;
+    bUseControllerRotationRoll = false;
+
+    //    FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
+    ////    FP_MuzzleLocation->SetupAttachment(FP_Gun);
+    //    FP_MuzzleLocation->SetRelativeLocation(FVector(42.0f, 40.0f, 40.0f));
 
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
 
-	// if (GEngine != nullptr)
-	// 		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Using PlayerCharacter."));
+    // if (GEngine != nullptr)
+    // 		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Using PlayerCharacter."));
 
 //	if (RootComponent != nullptr) {
 //		Camera = GetOwner()->FindComponentByClass<UCameraComponent>();
-		// Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-		// Camera->AttachTo(RootComponent);
+        // Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+        // Camera->AttachTo(RootComponent);
 //	}
-	
-	
+
+    Camera = FindComponentByClass<UCameraComponent>();
+
+
 }
 
 // Called every frame
 void APlayerCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 
 }
 
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	// Handles player movement
-	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("Strafe", this, &APlayerCharacter::Strafe);
-	PlayerInputComponent->BindAxis("Spin", this, &APlayerCharacter::Spin);
-	// PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::HorizontalRotation);
-	// PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::AddControllerYawInput);
-	// // PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::VerticalRotation);
-	// PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::AddControllerYawInput);
-    PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("Ascend", this, &APlayerCharacter::Ascend);
-    
+    // Handles player movement
+    PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("Strafe", this, &APlayerCharacter::Strafe);
+    PlayerInputComponent->BindAxis("Spin", this, &APlayerCharacter::Spin);
+    PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::HorizontalRotation);
+    // PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::AddControllerYawInput);
+    PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::VerticalRotation);
+    // PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::AddControllerPitchInput);
+    //PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::AddControllerYawInput);
+    //PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis("Ascend", this, &APlayerCharacter::Ascend);
+
     // Bind fire event
     PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &APlayerCharacter::OnFire);
 }
 
 void APlayerCharacter::OnFire()
 {
-//    UE_LOG(LogTemp, Warning, TEXT("FIRING"));
-        // Attempt to fire a projectile.
-    if (ProjectileClass)
+    // Attempt to fire a projectile.
+    if (ProjectileClass && !isDead)
     {
         // Get the camera transform.
-        FVector CameraLocation;
-        FRotator CameraRotation;
-        GetActorEyesViewPoint(CameraLocation, CameraRotation);
+        FVector ForwardVector = Camera->GetForwardVector();
+        FVector CameraLocation = Camera->GetComponentLocation();// GetActorLocation();
+        FRotator CameraRotation = Camera->GetComponentRotation();//GetActorRotation();
+        // GetActorEyesViewPoint(CameraLocation, CameraRotation);
 
-        // Set MuzzleOffset to spawn projectiles slightly in front of the camera.
-//        MuzzleOffset.Set(0.01f, 0.0f, 0.0f);
-
-        // Transform MuzzleOffset from camera space to world space.
-        FVector MuzzleLocation = CameraLocation + (-50.0f, -25.0f, -50.0f);
-        
-//        FVector MuzzleLocation = CameraLocation;
-        
-        // Skew the aim to be slightly upwards.
+        // Move the start location to in front of the head
+        FVector MuzzleLocation = CameraLocation + ForwardVector * 80;
         FRotator MuzzleRotation = CameraRotation;
-        MuzzleRotation.Pitch += 10.0f;
 
         UWorld* World = GetWorld();
         if (World)
@@ -104,9 +98,11 @@ void APlayerCharacter::OnFire()
             FActorSpawnParameters SpawnParams;
             SpawnParams.Owner = this;
             SpawnParams.Instigator = GetInstigator();
+            AMyProjectile* Projectile = World->SpawnActor<AMyProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
+            // Set the projectile's initial trajectory.
+            FVector LaunchDirection = MuzzleRotation.Vector();
+            Projectile->FireInDirection(LaunchDirection);
 
-            
-            
             if (!HasAuthority())
             {
                 //On the Client
@@ -114,20 +110,12 @@ void APlayerCharacter::OnFire()
             }
             else
             {
-                //On the Server
-                // Spawn the projectile at the muzzle.
-                AMyProjectile* Projectile = World->SpawnActor<AMyProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
-                if (Projectile)
-                {
-                    // Set the projectile's initial trajectory.
-                    FVector LaunchDirection = MuzzleRotation.Vector();
-                    Projectile->FireInDirection(LaunchDirection);
-                }
+                // Called from the server
+                Multi_OnFire(MuzzleLocation, MuzzleRotation);
             }
         }
     }
 }
-
 
 bool APlayerCharacter::Server_OnFire_Validate(FVector Location, FRotator Rotation)
 {
@@ -136,133 +124,314 @@ bool APlayerCharacter::Server_OnFire_Validate(FVector Location, FRotator Rotatio
 
 void APlayerCharacter::Server_OnFire_Implementation(FVector Location, FRotator Rotation)
 {
-    //Since this is done on the server, All the clients should see this happen as well.
-    
-    UE_LOG(LogTemp, Warning, TEXT("Server OnFire Implementation has been Called!"));
-    
-    FActorSpawnParameters SpawnParams;
-    SpawnParams.Owner = this;
-    SpawnParams.Instigator = GetInstigator();
-    AMyProjectile* Projectile = GetWorld()->SpawnActor<AMyProjectile>(ProjectileClass, Location, Rotation, SpawnParams);
-    if (Projectile)
+    Multi_OnFire(Location, Rotation);
+}
+
+bool APlayerCharacter::Multi_OnFire_Validate(FVector Location, FRotator Rotation)
+{
+    return true;
+}
+
+void APlayerCharacter::Multi_OnFire_Implementation(FVector Location, FRotator Rotation)
+{
+    if (!IsLocallyControlled()) {
+        FActorSpawnParameters SpawnParams;
+        SpawnParams.Owner = this;
+        SpawnParams.Instigator = GetInstigator();
+        AMyProjectile* Projectile = GetWorld()->SpawnActor<AMyProjectile>(ProjectileClass, Location, Rotation, SpawnParams);
+        if (Projectile && !isDead)
         {
             // Set the projectile's initial trajectory.
             FVector LaunchDirection = Rotation.Vector();
             Projectile->FireInDirection(LaunchDirection);
         }
-
-    
-    
-    //MultiCase Version
-//    Multi_OnFire(Location, Rotation);
-    
+    }
 }
-
-//void APlayerCharacter::OnFire()
-//{
-//    UE_LOG(LogTemp, Warning, TEXT("Function Called"));
-//    // try and fire a projectile
-//    if (ProjectileClass != nullptr)
-//    {
-//        UE_LOG(LogTemp, Warning, TEXT("passed first if Called"));
-//        UWorld* const World = GetWorld();
-//        if (World != nullptr)
-//        {
-//                const FRotator SpawnRotation = GetControlRotation();
-//                // MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
-////                const FVector SpawnLocation = ((FP_MuzzleLocation != nullptr) ? FP_MuzzleLocation->GetComponentLocation() : GetActorLocation()) + SpawnRotation.RotateVector(GunOffset);
-//                const FVector SpawnLocation(90.f, 40.0f, 50.0f);
-//                FActorSpawnParameters ActorSpawnParams;
-////                ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
-//                ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-//                World->SpawnActor<AMyProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
-//
-//            UE_LOG(LogTemp, Warning, TEXT("Inside Big If"));
-//
-//        }
-//    }
-//
-//    // try and play the sound if specified
-//    if (FireSound != nullptr)
-//    {
-//        UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
-//    }
-//
-////    // try and play a firing animation if specified
-////    if (FireAnimation != nullptr)
-////    {
-////        // Get the animation object for the arms mesh
-////        UAnimInstance* AnimInstance = Mesh1P->GetAnimInstance();
-////        if (AnimInstance != nullptr)
-////        {
-////            AnimInstance->Montage_Play(FireAnimation, 1.f);
-////        }
-////    }
-//}
-
 
 // Player moves forwards or backwards
 // Val is the movement amount input
 void APlayerCharacter::MoveForward(float Val)
 {
-	FVector Direction = GetActorForwardVector();//FRotationMatrix(Controller->GetActorForwardVector()).GetScaledAxis(EAxis::X);
-    // UE_LOG(LogTemp, Warning, TEXT("Forward is %d %d %d"), Direction.X, Direction.Y, Direction.Z);
-//
-	AddMovementInput(Direction, Val);
+    if (!isDead) {
+        FVector Direction = GetActorForwardVector();
+        FVector Position = GetActorLocation();
+
+        //SetActorLocation(Position + Direction * Val * ForwardSpeed, true);
+        AddMovementInput(Direction, Val);
+
+        if (!HasAuthority())
+        {
+            //On the Client
+            Server_MoveForward(Val);
+        }
+        else
+        {
+            // Called from the server
+            Multi_MoveForward(Val);
+        }
+    }
+
+}
+
+bool APlayerCharacter::Server_MoveForward_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Server_MoveForward_Implementation(float Val)
+{
+    Multi_MoveForward(Val);
+}
+
+bool APlayerCharacter::Multi_MoveForward_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Multi_MoveForward_Implementation(float Val)
+{
+    if (!IsLocallyControlled()) {
+        FVector Direction = GetActorForwardVector();
+        FVector Position = GetActorLocation();
+
+        AddMovementInput(Direction, Val);
+        //SetActorLocation(Position + Direction * Val * ForwardSpeed, true);
+        // * Val * ForwardSpeed * GetWorld()->GetDeltaSeconds()
+    }
 }
 
 // Player moves left or right
 void APlayerCharacter::Strafe(float Val)
 {
-	// FVector Direction = GetActorRightVector();	//FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
-    // AddMovementInput(Direction, Val);
-	const FRotator Rotation = GetControlRotation();
-	// const FRotator YawRotation(0, Rotation.Yaw, 0);
+    if (!isDead) {
+        // Get right vector
+        const FVector Direction = GetActorRightVector();
+        FVector Position = GetActorLocation();
 
-	// Get right vector
-	const FVector Direction = FRotationMatrix(Rotation).GetUnitAxis(EAxis::Y);
-	    // UE_LOG(LogTemp, Warning, TEXT("Right is %d %d %d"), Direction.X, Direction.Y, Direction.Z);
+        AddMovementInput(Direction, Val);
+        //SetActorLocation(Position + Direction * Val * StrafeSpeed, true);
 
+        if (!HasAuthority())
+        {
+            //On the Client
+            Server_Strafe(Val);
+        }
+        else
+        {
+            // Called from the server
+            Multi_Strafe(Val);
+        }
+    }
 
-	// add movement in that direction
-	AddMovementInput(Direction, Val);
+}
+
+bool APlayerCharacter::Server_Strafe_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Server_Strafe_Implementation(float Val)
+{
+    Multi_Strafe(Val);
+}
+
+bool APlayerCharacter::Multi_Strafe_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Multi_Strafe_Implementation(float Val)
+{
+    if (!IsLocallyControlled()) {
+        const FVector Direction = GetActorRightVector();
+        FVector Position = GetActorLocation();
+        
+        AddMovementInput(Direction, Val);
+        //SetActorLocation(Position + Direction * Val * StrafeSpeed, true);
+        // * Val * StrafeSpeed * GetWorld()->GetDeltaSeconds()
+    }
 }
 
 void APlayerCharacter::Ascend(float Val)
 {
-	FVector Direction = GetActorUpVector();
-	AddMovementInput(Direction, Val);
+    if (!isDead) {
+        FVector Position = GetActorLocation();
+        FVector Direction = GetActorUpVector();
 
+        AddMovementInput(Direction, Val);
+        //SetActorLocation(Position + Direction * Val * AscendSpeed, true);
+
+        if (!HasAuthority())
+        {
+            //On the Client
+            Server_Ascend(Val);
+        }
+        else
+        {
+            // Called from the server
+            Multi_Ascend(Val);
+        }
+    }
+
+}
+
+bool APlayerCharacter::Server_Ascend_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Server_Ascend_Implementation(float Val)
+{
+    Multi_Ascend(Val);
+}
+
+bool APlayerCharacter::Multi_Ascend_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Multi_Ascend_Implementation(float Val)
+{
+    if (!IsLocallyControlled()) {
+        FVector Position = GetActorLocation();
+        FVector Direction = GetActorUpVector();
+
+        AddMovementInput(Direction, Val);
+        //SetActorLocation(Position + Direction * Val * AscendSpeed, true);
+        //*Val* AscendSpeed* GetWorld()->GetDeltaSeconds()
+    }
 }
 
 // Player spins left or right
 void APlayerCharacter::Spin(float Val)
 {
-	// if (Val)
-		// UE_LOG(LogTemp, Warning, TEXT("val is %d"), Val);
-	AddControllerRollInput(Val);
-	if (Val)
-		AddActorLocalRotation(FQuat(FRotator(0, 0, Val)));
+    if (!isDead) {
+        AddActorLocalRotation(FQuat(FRotator(0, 0, Val)));
+        // * SpinSpeed * GetWorld()->GetDeltaSeconds()
+
+        if (!HasAuthority())
+        {
+            //On the Client
+            Server_Spin(Val);
+        }
+        else
+        {
+            // Called from the server
+            Multi_Spin(Val);
+        }
+    }
+
+}
+
+bool APlayerCharacter::Server_Spin_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Server_Spin_Implementation(float Val)
+{
+    Multi_Spin(Val);
+}
+
+bool APlayerCharacter::Multi_Spin_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Multi_Spin_Implementation(float Val)
+{
+    if (!IsLocallyControlled()) {
+        AddActorLocalRotation(FQuat(FRotator(0, 0, Val)));
+        // * SpinSpeed * GetWorld()->GetDeltaSeconds()
+    }
 }
 
 void APlayerCharacter::HorizontalRotation(float Val)
 {
-	// UE_LOG(LogTemp, Warning, TEXT("left is %d"), Val);
+    if (!isDead) {
+        UE_LOG(LogTemp, Warning, TEXT("left is %d"), Val);
 
-		AddControllerYawInput(Val);
-		// AddActorLocalRotation(FQuat(FRotator(0, Val, 0)));
+        AddActorLocalRotation(FQuat(FRotator(0, Val, 0)));
 
-		// add this later GetWorld()->GetDeltaSeconds()
+        // add this later GetWorld()->GetDeltaSeconds()
+
+        if (!HasAuthority())
+        {
+            //On the Client
+            Server_HorizontalRotation(Val);
+        }
+        else
+        {
+            // Called from the server
+            Multi_HorizontalRotation(Val);
+        }
+    }
+
 }
+
+bool APlayerCharacter::Server_HorizontalRotation_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Server_HorizontalRotation_Implementation(float Val)
+{
+    Multi_HorizontalRotation(Val);
+}
+
+bool APlayerCharacter::Multi_HorizontalRotation_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Multi_HorizontalRotation_Implementation(float Val)
+{
+    if (!IsLocallyControlled()) {
+        AddActorLocalRotation(FQuat(FRotator(0, Val, 0)));
+        // * MouseSensitivity * GetWorld()->GetDeltaSeconds()
+    }
+}
+
 
 void APlayerCharacter::VerticalRotation(float Val)
 {
-	// UE_LOG(LogTemp, Warning, TEXT("right is is %d"), Val);
+    if (!isDead) {
+        Val *= -1;
+        AddActorLocalRotation(FQuat(FRotator(Val, 0, 0)));
 
-	// if (Val)
-	AddControllerPitchInput(Val);
-		// AddActorLocalRotation(FQuat(FRotator(Val, 0, 0)));
+        if (!HasAuthority())
+        {
+            //On the Client
+            Server_VerticalRotation(Val);
+        }
+        else
+        {
+            // Called from the server
+            Multi_VerticalRotation(Val);
+        }
+    }
+
 }
 
+bool APlayerCharacter::Server_VerticalRotation_Validate(float Val)
+{
+    return true;
+}
 
+void APlayerCharacter::Server_VerticalRotation_Implementation(float Val)
+{
+    Multi_VerticalRotation(Val);
+}
+
+bool APlayerCharacter::Multi_VerticalRotation_Validate(float Val)
+{
+    return true;
+}
+
+void APlayerCharacter::Multi_VerticalRotation_Implementation(float Val)
+{
+    if (!IsLocallyControlled()) {
+        AddActorLocalRotation(FQuat(FRotator(Val, 0, 0)));
+    }
+}
 
--- Source/Comp460_UE4/PlayerCharacter.h
+++ Source/Comp460_UE4/PlayerCharacter.h
@@ -16,81 +16,162 @@ class USoundBase;
 UCLASS()
 class COMP460_UE4_API APlayerCharacter : public ACharacter
 {
-	GENERATED_BODY()
-    
-//    /** Location on gun mesh where projectiles should spawn. */
-//    UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
-//    USceneComponent* FP_MuzzleLocation;
+    GENERATED_BODY()
+
+        //    /** Location on gun mesh where projectiles should spawn. */
+        //    UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+        //    USceneComponent* FP_MuzzleLocation;
 
 public:
-	// Sets default values for this character's properties
-	APlayerCharacter();
+    // Sets default values for this character's properties
+    APlayerCharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-    
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
     // Projectile class to spawn.
     UPROPERTY(EditDefaultsOnly, Category = Projectile)
-    TSubclassOf<class AMyProjectile> ProjectileClass;
+        TSubclassOf<class AMyProjectile> ProjectileClass;
 
 public:
-    
+
     /** Gun muzzle's offset from the characters location */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
     FVector MuzzleOffset;
 
 
     /** Sound to play each time we fire */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
     USoundBase* FireSound;
 
     /** AnimMontage to play each time we fire */
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
     UAnimMontage* FireAnimation;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerSettings)
+    bool isDead = false;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerSettings)
+    int Damage = 25;
+
+    // Movement speeds
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerSettings)
+    float MouseSensitivity = 50.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerSettings)
+    float AscendSpeed = 10.0f;
     
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerSettings)
+    float StrafeSpeed = 10.0f;
     
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerSettings)
+    float ForwardSpeed = 10.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerSettings)
+    float SpinSpeed = 10.0f;
+
+
     //Function handling firing projectiles
     UFUNCTION()
-    void OnFire();
-    
+        void OnFire();
+
     UFUNCTION(Server, Reliable, WithValidation)
     void Server_OnFire(FVector Location, FRotator Rotation);
     bool Server_OnFire_Validate(FVector Location, FRotator Rotation);
     void Server_OnFire_Implementation(FVector Location, FRotator Rotation);
-    
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    UFUNCTION(NetMulticast, Reliable, WithValidation)
+    void Multi_OnFire(FVector Location, FRotator Rotation);
+    bool Multi_OnFire_Validate(FVector Location, FRotator Rotation);
+    void Multi_OnFire_Implementation(FVector Location, FRotator Rotation);
 
-//protected:
-//        /** Fires a projectile. */
-//    void OnFire();
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
-	// Handles forward/backward movement input
-	UFUNCTION()
-	void MoveForward(float Val);
+    // Handles forward/backward movement input
+    UFUNCTION()
+    void MoveForward(float Val);
 
-	UFUNCTION()
-	void Strafe(float Val);
+    UFUNCTION(Server, Reliable, WithValidation)
+    void Server_MoveForward(float Val);
+    bool Server_MoveForward_Validate(float Val);
+    void Server_MoveForward_Implementation(float Val);
 
-	UFUNCTION()
-	void Ascend(float Val);
+    UFUNCTION(NetMulticast, Reliable, WithValidation)
+    void Multi_MoveForward(float Val);
+    bool Multi_MoveForward_Validate(float Val);
+    void Multi_MoveForward_Implementation(float Val);
 
-	UFUNCTION()
-	void Spin(float Val);
+    UFUNCTION()
+    void Strafe(float Val);
 
-	UFUNCTION()
-	void HorizontalRotation(float Val);
+    UFUNCTION(Server, Reliable, WithValidation)
+    void Server_Strafe(float Val);
+    bool Server_Strafe_Validate(float Val);
+    void Server_Strafe_Implementation(float Val);
 
-	UFUNCTION()
-	void VerticalRotation(float Val);
+    UFUNCTION(NetMulticast, Reliable, WithValidation)
+    void Multi_Strafe(float Val);
+    bool Multi_Strafe_Validate(float Val);
+    void Multi_Strafe_Implementation(float Val);
 
-	UPROPERTY()
-	UCameraComponent* Camera;
+    UFUNCTION()
+    void Ascend(float Val);
 
-    
+    UFUNCTION(Server, Reliable, WithValidation)
+    void Server_Ascend(float Val);
+    bool Server_Ascend_Validate(float Val);
+    void Server_Ascend_Implementation(float Val);
+
+    UFUNCTION(NetMulticast, Reliable, WithValidation)
+    void Multi_Ascend(float Val);
+    bool Multi_Ascend_Validate(float Val);
+    void Multi_Ascend_Implementation(float Val);
+
+    UFUNCTION()
+    void Spin(float Val);
+
+    UFUNCTION(Server, Reliable, WithValidation)
+    void Server_Spin(float Val);
+    bool Server_Spin_Validate(float Val);
+    void Server_Spin_Implementation(float Val);
+
+    UFUNCTION(NetMulticast, Reliable, WithValidation)
+    void Multi_Spin(float Val);
+    bool Multi_Spin_Validate(float Val);
+    void Multi_Spin_Implementation(float Val);
+
+    UFUNCTION()
+    void HorizontalRotation(float Val);
+
+    UFUNCTION(Server, Reliable, WithValidation)
+    void Server_HorizontalRotation(float Val);
+    bool Server_HorizontalRotation_Validate(float Val);
+    void Server_HorizontalRotation_Implementation(float Val);
+
+    UFUNCTION(NetMulticast, Reliable, WithValidation)
+    void Multi_HorizontalRotation(float Val);
+    bool Multi_HorizontalRotation_Validate(float Val);
+    void Multi_HorizontalRotation_Implementation(float Val);
+
+    UFUNCTION()
+    void VerticalRotation(float Val);
+
+    UFUNCTION(Server, Reliable, WithValidation)
+    void Server_VerticalRotation(float Val);
+    bool Server_VerticalRotation_Validate(float Val);
+    void Server_VerticalRotation_Implementation(float Val);
+
+    UFUNCTION(NetMulticast, Reliable, WithValidation)
+    void Multi_VerticalRotation(float Val);
+    bool Multi_VerticalRotation_Validate(float Val);
+    void Multi_VerticalRotation_Implementation(float Val);
+
+    UPROPERTY()
+    UCameraComponent* Camera;
 };
